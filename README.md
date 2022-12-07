44537ce4fee9b65153e764de19f2a111e8a8a478
TheCondemner <1tymur.tykva1@gmail.com>
2022-12-07 16:19:18 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Point Break
+
+## Features
+- Asset LOD; graphics settings tied into Roblox graphics level & individual override (LOD, Shadows, view distance, etc.)
+- Lag-reduction by only rendering objects in player FOV
+- Custom char. controller; wallrunning, etc.
+- FPS system
+- PvE; Wave-based shooter
+- PvP; 'training' mode?
---
+++ src/Server/meshing.server.lua
@@ -0,0 +1,15 @@
+--[[
+    Adds support for cross-server data streaming.
+--]]
+
+-- ------------------------------ Dependencies ------------------------------ --
+
+
+-- ----------------------- Services & Primary Objects ----------------------- --
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+-- ---------------------------- Config Variables ---------------------------- --
+
+
+-- -------------------------------- Main Code ------------------------------- --
